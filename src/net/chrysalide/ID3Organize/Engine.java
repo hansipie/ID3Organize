915bb905ab63476f6c5c068631f85419453a808b
hansipie <hansipie@gmail.com>
2013-11-03 02:40:00 +0100

    - add progression display - exclude (, ), [, ] and . from string replacement

--- src/net/chrysalide/ID3Organize/Engine.java
+++ src/net/chrysalide/ID3Organize/Engine.java
@@ -17,7 +17,9 @@ public class Engine {
 	private File _sourceDir;
 	private File _destinationDir;
 	private List<String> _strError;
-	
+	private int _totalCount;
+	private int _prevPerCent;
+
 
 	public Engine(File srcDir, File destDir) {
 		_sourceDir = srcDir;
@@ -25,16 +27,20 @@ public class Engine {
 		_count = 0;
 		_error = 0;
 		_strError = new ArrayList<String>();
+		_totalCount = 0;
+		_prevPerCent = 0;
 	}
 
 	public boolean directoryCrawler() {
 
+		_totalCount = directoryCount(_sourceDir, _totalCount);
+		System.out.println(String.format("Total Items count: %1$d", _totalCount));
 		boolean ret = directoryCrawler(_sourceDir);
 		System.out.println("-- Rapport --");
 		for (String line : _strError) {
 			System.out.println("error: " + line);
 		}
-		System.out.println(String.format("\ntotal:%1$d - erreurs:%2$d", _count,
+		System.out.println(String.format("\ntotal: %1$d - erreurs: %2$d", _count,
 				_error));
 		return ret;
 	}
@@ -55,16 +61,31 @@ public class Engine {
 		return true;
 	}
 
+	private int directoryCount(File input, int count) {
+		if (!input.isDirectory())
+			return count++;
+
+		for (File file : input.listFiles()) {
+			if (file.isDirectory()) {
+				count = directoryCount(file, count);
+			} else {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	public boolean copyMp3(File inputFile) {
 
 		String album;
 		String artist;
 		String title;
 
-		_count += 1;
+		_count++;
+		progressCounter();
 		try {
 			MP3File mp3File = new MP3File(inputFile);
-			
+
 			ID3v1 id3v1 = mp3File.getID3v1Tag();
 			AbstractID3v2 id3v2 = mp3File.getID3v2Tag();
 
@@ -106,17 +127,17 @@ public class Engine {
 
 		String path = _destinationDir + File.separator + artist
 				+ File.separator + album + File.separator + inputFile.getName();
-		
+
 		File dest = new File(path);
 		writeInPath(inputFile, dest);
 		return true;
 	}
 
 	private String validateStringForFilename(String input){
-		input = input.replaceAll("[^\\w\\s]", "_");
+		input = input.replaceAll("[^\\w\\s\\[\\]\\(\\),\\.]", "_");
 		return input;
 	}
-	
+
 	private boolean writeInPath(File file, File dest) {
 		try {
 			Files.createDirectories(dest.getParentFile().toPath());
@@ -125,5 +146,13 @@ public class Engine {
 			return false;
 		}
 		return true;
+	}
+	
+	private void progressCounter(){
+		int perCent = (_count * 100) / _totalCount;
+		if (perCent != _prevPerCent){
+			System.out.println(String.format("Progress: %1$d", perCent)+"%");
+		}
+		_prevPerCent = perCent;
 	}
 }
